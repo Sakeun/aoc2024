064081d9e94c3080e7e34d35c9cf459e9e32a8e6
SietseEurostocks <49184286+Sakeun@users.noreply.github.com>
2024-12-04 18:33:20 +0100

    temp day 2 update

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	fmt.Printf("Puzzle one: %v \n", week1.PuzzleOne())
 	fmt.Printf("Puzzle two: %v \n", week1.PuzzleTwo())
 
+	fmt.Println("Day 2:")
+	fmt.Printf("Puzzle one: %v \n", week1.DayTwoPuzzleOne())
+	fmt.Printf("Puzzle two: %v \n", week1.DayTwoPuzzleTwo())
+
 	fmt.Printf("Puzzle one: %v \n", week1.Day3Part1())
 	fmt.Printf("Puzzle two: %v \n", week1.Day3Part2())
 
--- week1/day2.go
+++ week1/day2.go
@@ -1,12 +1,11 @@
 package week1
 
 import (
-	"os"
 	"strconv"
 	"strings"
 )
 
-const input = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"
+const input = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\n5 9 4 3 2"
 
 func parseInputDay2() [][]int {
 	rows := [][]int{}
@@ -14,9 +13,9 @@ func parseInputDay2() [][]int {
 
 	var currNumBuilder strings.Builder
 
-	content, _ := os.ReadFile("week1/inputs/day2.txt")
+	//content, _ := os.ReadFile("week1/inputs/day2.txt")
 
-	for _, val := range content {
+	for _, val := range input {
 		if val == 10 {
 			num, _ := strconv.Atoi(currNumBuilder.String())
 			row = append(row, num)
@@ -38,6 +37,14 @@ func parseInputDay2() [][]int {
 		currNumBuilder.WriteString(string(val))
 	}
 
+	num, _ := strconv.Atoi(currNumBuilder.String())
+	row = append(row, num)
+
+	rows = append(rows, row)
+
+	currNumBuilder.Reset()
+	row = []int{}
+
 	return rows
 }
 
@@ -52,6 +59,15 @@ func checkSafe(isInc bool, row []int, i int) bool {
 		return row[i]-row[i+1] <= 3
 	}
 
+}
+
+func checkIncSafe(isInc bool, row []int, i int) bool {
+	if isInc && row[i] > row[i+1] {
+		return false
+	} else if !isInc && row[i] < row[i+1] {
+		return false
+	}
+	return true
 }
 
 func DayTwoPuzzleOne() int {
@@ -92,6 +108,10 @@ func DayTwoPuzzleOne() int {
 	return amountSafe
 }
 
+func remove(slice []int, i int) []int {
+	return append(slice[:i+1], slice[i+2:]...)
+}
+
 func DayTwoPuzzleTwo() int {
 
 	rows := parseInputDay2()
@@ -102,31 +122,31 @@ func DayTwoPuzzleTwo() int {
 		isSafe := false
 		passUsed := false
 
-		for i, num := range row {
+		for i := 0; i <= len(row); {
+			if i+1 >= len(row) {
+				break
+			}
+
+			if i != 0 && !passUsed && (!checkIncSafe(isInc, row, i) || !checkSafe(isInc, row, i)) {
+				passUsed = true
+				row = remove(row, i)
+				continue
+			}
+
 			if i+1 >= len(row) {
 				break
 			}
 
 			if i == 0 {
-				isInc = num < row[i+1]
-			} else if isInc && num > row[i+1] {
-				isSafe = false
-				break
-			} else if !isInc && num < row[i+1] {
-				isSafe = false
-				break
+				isInc = row[i] < row[i+1]
 			}
 
-			isSafe = checkSafe(isInc, row, i)
+			isSafe = checkSafe(isInc, row, i) || checkIncSafe(isSafe, row, i)
 
 			if !isSafe {
-				if !passUsed {
-					passUsed = true
-					continue
-				}
-
 				break
 			}
+			i++
 		}
 
 		if isSafe {
